54845c62e63aa09c3cb2e518fadca240a8aea31c
Aditya <adityakashyap2501@gmail.com>
2025-06-29 19:26:04 +0530

    Implement Hero Section and NavBar with animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import gsap from 'gsap'
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-centerx h-[100vh]'>
-      <div>Hello world</div>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
+    </main>
   )
 }
 
