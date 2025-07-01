f5e4bf9b423accfca998e4f831cca80dd7e0172c
Aditya <adityakashyap2501@gmail.com>
2025-07-01 11:47:31 +0530

    change image src

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -27,13 +27,13 @@ const Cocktails = () => {
   return (
     <section id='cocktails' className='noisy'>
       <img
-        src='/public/images/cocktail-left-leaf.png'
+        src='/images/cocktail-left-leaf.png'
         alt='left-leaf'
         id='c-left-leaf'
       />
 
       <img 
-        src='/public/images/cocktail-right-leaf.png'
+        src='/images/cocktail-right-leaf.png'
         alt='right-leaf'
         id='c-right-leaf'
       />
